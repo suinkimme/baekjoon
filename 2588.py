9f3a42f4b861f2bd275c30063a9a04a40285ec8e
Suin Kim <contact@suinkim.me>
2025-05-18 21:25:24 +0900

    정답 주석 작성

--- 2588.py
+++ 2588.py
@@ -1,6 +1,7 @@
 a = int(input())
-b = int(input())
+b = int(input()) # 굳이 int로 받을 필요가 없었네
 
+# int로 안받았으면 형태 변환할 필요 없었음
 c = int(b / 100)
 d = int((b - c * 100) / 10)
 e = int(b - (c * 100 + d * 10))
