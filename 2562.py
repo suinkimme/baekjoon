86b3c86b1fe580235414f2031e7c74f9cf9b55ba
Suin Kim <contact@suinkim.me>
2025-05-19 00:58:31 +0900

    2562 설명 추가

--- 2562.py
+++ 2562.py
@@ -5,3 +5,10 @@ for i in range(9):
     a = (n, i + 1)
 
 print(a[0], a[1], sep='\n')
+
+"""
+numbers = [int(input()) for _ in range(9)]
+
+print(max(numbers)) # 가장 큰 값 출력
+print(numbers.index(max(numbers)) + 1) # 값으로 인덱스를 찾는건데 같은 값이 있으면 어떻게 처리하지? -> 처음 값을 반환함
+"""
