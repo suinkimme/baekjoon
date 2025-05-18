cc9e12058bd3fc9fe9b88347eda41aa8218839f7
Suin Kim <contact@suinkim.me>
2025-05-18 21:23:31 +0900

    정답 코드 추가

--- 2588.py
+++ 2588.py
@@ -9,3 +9,15 @@ print(a * e)
 print(a * d)
 print(a * c)
 print(a * b)
+
+"""
+a = int(input())
+b = input()
+
+a1 = a * int(b[2])
+a2 = a * int(b[1])
+a3 = a * int(b[0])
+a4 = a * int(b)
+
+print (a1, a2, a3, a4, sep='\n')
+"""
