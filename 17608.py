0a21c3d7943a6598d7ee829a7f423b80c2465e27
Suin Kim <contact@suinkim.me>
2025-05-23 19:35:06 +0900

    설명 추가

--- 17608.py
+++ 17608.py
@@ -13,6 +13,6 @@ highest = S[0]
 for i in range(1, len(S)):
   if S[i] > highest:
     count += 1
-    highest = S[i]
+    highest = S[i] # 높은 수 찾고 높은 수를 업데이트 해두면 그 뒤 숫자를 카운트 안함;; 와 이걸 생각못했네
 
 print(count)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ > max_index:
+    count += 1
