b5aeb5c7a0ab9cb878141a8e80678ac4615e84a4
Suin Kim <contact@suinkim.me>
2025-06-03 17:27:02 +0900

    이진 검색 트리

---
+++ 5639.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+pre = []
+while True:
+  try:
+    pre.append(int(input()))
+  except:
+    break
+
+def post(start, end):
+  if start > end:
+    return
+  
+  mid = end + 1
+  for i in range(start + 1, end + 1):
+    if pre[i] > pre[start]:
+      mid = i
+      break
+  
+  post(start + 1, mid - 1)
+  post(mid, end)
+  print(pre[start])
+
+post(0, len(pre) - 1)
