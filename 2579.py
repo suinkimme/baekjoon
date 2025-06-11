90b0ad54bfe6b8a231a040d6cd7cd2890fb1e885
Suin Kim <contact@suinkim.me>
2025-06-12 02:05:53 +0900

    계단 오르기 - 20250612

--- 2579.py
+++ 2579.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 
 n = int(input())
 
-stairs = [0] * 301
-for i in range(1, n + 1):
-  stairs[i] = int(input())
+stairs = [0]
+for _ in range(n):
+  stairs.append(int(input()))
 
-dp = [0] * 301
-dp[1] = stairs[1]
-dp[2] = stairs[1] + stairs[2]
-dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+memo = [0] * 301
+memo[1] = stairs[1]
+memo[2] = stairs[1] + stairs[2]
+memo[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
 
 for i in range(4, n + 1):
-  dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i])
+  memo[i] = max(memo[i - 3] + stairs[i - 1] + stairs[i], memo[i - 2] + stairs[i])
 
-print(dp[n])
+print(memo[n])
