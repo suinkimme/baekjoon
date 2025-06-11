5678e872409626a8ef12834f30ec53aa3e09fb90
Suin Kim <contact@suinkim.me>
2025-06-11 21:07:23 +0900

    LCS-20250611

---
+++ 9251-20250611.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+s1 = str(input())
+s2 = str(input())
+
+n = len(s1)
+m = len(s2)
+
+memo = [[0] * (n + 1) for _ in range(m + 1)]
+
+for i in range(1, n):
+  for j in range(1, m):
+    if s1[i - 1] == s2[j - 1]:
+      memo[i][j] = memo[i - 1][j - 1] + 1
+    else:
+      memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+      
+
+print(memo[i][j])
