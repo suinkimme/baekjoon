016127fdf2bb4350a6fd70c5ace0b223e9b4afe0
Suin Kim <contact@suinkim.me>
2025-06-11 21:43:36 +0900

    반복 range 수정

--- 9251-20250611.py
+++ 9251-20250611.py
@@ -9,8 +9,8 @@ m = len(s2)
 
 memo = [[0] * (n + 1) for _ in range(m + 1)]
 
-for i in range(1, n):
-  for j in range(1, m):
+for i in range(1, n + 1):
+  for j in range(1, m + 1):
     if s1[i - 1] == s2[j - 1]:
       memo[i][j] = memo[i - 1][j - 1] + 1
     else:
