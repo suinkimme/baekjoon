cdc9e45ee49de478796c38e3706d6a58a7b6952c
Suin Kim <contact@suinkim.me>
2025-06-12 10:44:44 +0900

    거스름돈

---
+++ 14916.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+count = 0
+while True:
+  if n % 5 == 0:
+    count += n // 5
+    break
+  else:
+    n -= 2
+    count += 1
+
+if n < 0:
+  print(-1)
+else:
+  print(count)
